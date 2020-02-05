3faf67332bf6c6c23fd36f6026b502454ec5b654
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-02-06 00:03:36 +0400

    POST & PUT added

--- Lesson5/express.js
+++ Lesson5/express.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const consolidate = require('consolidate');
 const mongoose = require('mongoose');
+const log = console.log
 
-mongoose.connect('mongodb://localhost:32776/insta', {
+mongoose.connect('mongodb://192.168.99.100:32768/insta', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -16,19 +17,14 @@ app.set('view engine', 'hbs');
 app.set('views', path.resolve(__dirname, 'views'));
 
 // Middleware
-// app.use('/users', express.json());
 app.use(express.json());
 // Необходимо для парсинга body в пост запросах. extended указывает на возможность парсть вложенные структуры.
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 // Опишем статику для стилей. Аналогично можно расписать для других статичных ресурсов.
 app.use('/styles', express.static(path.resolve(__dirname, 'assets/css')));
 
-// написание собственного middleware
-app.use((req, res, next) => {
-    console.log('middleware');
-    req.user = { name: 'John' };
-    next(); // res.redirect('/...');
-});
 
 // Роутинг
 // описываем общий обработчик для всех роутов
@@ -37,18 +33,14 @@ app.all('/', (req, res, next) => {
     next();
 });
 app.get('/', (req, res) => {
-    // console.log('Hello World');
-    // res.send('Hello World!');
-    // res.json({ message: 'Hello World!' });
-    // res.send('<h2>Hello World!</h2>');
     res.sendFile(path.resolve(__dirname, 'index.html'));
 });
-app.get('/user', (req, res) => {
-    res.render('user', {
-        fullName: 'John Snow',
-        achievements: ['Главный сторож стены', 'Winter is coming!'],
-    });
-});
+// app.get('/user', (req, res) => {
+//     res.render('user', {
+//         fullName: 'John Snow',
+//         achievements: ['Главный сторож стены', 'Winter is coming!'],
+//     });
+// });
 
 const User = require('./models/user');
 
@@ -63,27 +55,45 @@ app.get('/users/:id', async (req, res) => {
 });
 
 app.post('/users', async (req, res) => {
+    if (Array.isArray(req.body)) {
+        let savedUsers = []
+        for (let i = 0; i < req.body.length; i++) {
+            const user = new User(req.body[i]);
+            const savedUser = await user.save();
+            savedUsers.push(savedUser);
+        }
+        res.json(savedUsers)
+
+    } else {
+        const user = new User(req.body);
+        const savedUser = await user.save();
+        res.json(savedUser);
+    }
+});
+
+app.put('/users', async (req, res) => {
     const user = new User(req.body);
-    const savedUser = await user.save();
-    res.json(savedUser);
-});
-// app.post('/users', async (req, res) => {
-//     if (Array.isArray(req.body)) {
-//         req.body.forEach((user) => {
-//             const user = new User(req.body);
-//             const savedUser = await user.save();
-//         });
-//     } else {
-//         const user = new User(req.body);
-//         const savedUser = await user.save();
-//         res.json(savedUser);
-//     }
-// });
-app.post('/tasks', (req, res) => {
-    console.log(req.body);
-    res.send('OK');
-});
+    const updatedUser = await user.update(user)
+    res.json(updatedUser);
+})
 
 app.listen(8000, () => {
     console.log('Server has been started!');
 });
+// app.post('/users', async (req, res) => {
+//     const user = new User(req.body);
+//     const savedUser = await user.save();
+//     res.json(savedUser);
+// });
+// app.post('/tasks', (req, res) => {
+//     console.log(req.body);
+//     res.send('OK');
+// });
+// // написание собственного middleware
+// app.use((req, res, next) => {
+//     console.log('middleware');
+//     req.user = {
+//         name: 'John'
+//     };
+//     next(); // res.redirect('/...');
+// });
