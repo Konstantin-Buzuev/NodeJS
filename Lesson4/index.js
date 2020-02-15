6e5db6a5ee6c84e615521c9a7e5d11d34b56db7b
Konstantin-Buzuev <55285803+Konstantin-Buzuev@users.noreply.github.com>
2020-02-15 17:21:25 +0400

    Homework#4 completed

--- Lesson4/index.js
+++ Lesson4/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const path = require('path')
 const consolidate = require('consolidate')
+const hbs = require('handlebars')
+const fs = require('fs')
+const request = require('request')
+const cheerio = require('cheerio')
 const log = console.log
 const app = express()
+let Agency = require('./models/agency')
 
 //MiddleWare
 app.engine('hbs', consolidate.handlebars)
 app.set('view engine', 'hbs')
+hbs.registerPartial('menu', fs.readFileSync(path.resolve(__dirname, 'views', 'partials', 'menu.hbs')).toString())
+hbs.registerPartial('news', fs.readFileSync(path.resolve(__dirname, 'views', 'partials', 'news.hbs')).toString())
 app.set('views', path.resolve(__dirname, "views"))
 // Для просмотра body
 app.use(express.urlencoded({
@@ -16,13 +23,45 @@ app.use(express.urlencoded({
 app.use(express.static('views'))
 app.use(express.static('assets'))
 
+
 app.get('/', (req, res) => {
-    res.render('view', {})
+    if (req.query.news === undefined) {
+        res.render('index', {})
+    } else grabNews(req.query).then(news => {
+        res.render('index', {
+            news: news
+        })
+    })
 })
 app.post('/', (req, res) => {
-    log(req.body)
-    res.render('view', {})
+    grabNews(req.body).then(news => {
+        res.render('index', {
+            news: news
+        })
+    })
+
 })
 app.listen(8000, () => {
     log('Server started!')
 })
+
+//Функция получения новостей
+let grabNews = function (params) {
+    return new Promise((resolve, reject) => {
+        let url = Agency[params.news].categories[params.category]
+        request(url, (err, request, body) => {
+            if (!err && request.statusCode === 200) {
+                const $ = cheerio.load(body)
+                const links = $(Agency[params.news].href[params.count])
+                const texts = $(Agency[params.news].text[params.count])
+                let news = []
+                for (let i = 0; i < links.length && i < texts.length; i++)
+                    news.push({
+                        text: texts[i].children[0].data,
+                        href: links[i].attribs.href
+                    })
+                resolve(news)
+            } else reject(err)
+        })
+    })
+}
